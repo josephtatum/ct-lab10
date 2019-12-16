4e7cadf0373e1481a63c0f39ce48fa5716fdcfe1
Joseph Tatum <josephtatum@gmail.com>
2019-12-16 14:28:56 -0800

    setup post route for recipes and created passing test for creating arecipe

--- __tests__/trips.test.js
+++ __tests__/trips.test.js
@@ -8,27 +8,59 @@ const Trip = require('../lib/models/Trip');
 const IteneraryItem = require('../lib/models/IteneraryItem');
 
 describe('app routes', () => {
+
   beforeAll(() => {
     connect();
-  });
-
-  let trip;
-  let iteneraryItem;
-  beforeEach(() => {
-    trip = new Trip({
-      origin: 'Portland, OR, USA',
-      destination: 'Stockholm, Sweden',
-      dateOfDeparture: new Date('2020-10-10'),
-      dateOfReturn: new Date('2020-10-30'),
-      modeOfTransit: 'airplane'
-    });
   });
 
   beforeEach(() => {
     return mongoose.connection.dropDatabase();
   });
 
+  let trip;
+  let iteneraryItem;
+  beforeEach(async() => {
+
+    trip = await Trip.create({
+      origin: 'Portland, OR, USA',
+      destination: 'Stockholm, Sweden',
+      dateOfDeparture: new Date('2020-10-10'),
+      dateOfReturn: new Date('2020-10-30'),
+      modeOfTransit: 'airplane'
+    });
+
+    iteneraryItem = await IteneraryItem.create({
+      tripId: trip._id,
+      name: 'Cafe Saturnus',
+      notes: 'eat a lot of cardamom rolls!'
+    });
+
+  });
+
   afterAll(() => {
     return mongoose.connection.close();
+  });
+
+  it('should be able to create a trip', () => {
+    return request(app)
+      .post('/api/v1/trips/')
+      .send({
+        origin: 'Portland, OR, USA',
+        destination: 'Stockholm, Sweden',
+        dateOfDeparture: new Date('2020-10-10'),
+        dateOfReturn: new Date('2020-10-30'),
+        modeOfTransit: 'airplane'
+      })
+      .then(response => {
+        expect(response.body).toEqual({
+          __v: 0,
+          _id: expect.any(String),
+          dateOfDeparture: expect.any(String),
+          destination: 'Stockholm, Sweden',
+          origin: 'Portland, OR, USA',
+          modeOfTransit: 'airplane',
+          dateOfReturn: expect.any(String)
+        });
+      });
   });
 });
