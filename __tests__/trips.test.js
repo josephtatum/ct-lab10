095e8c4738a91813f043bf06309763ae719e692f
Joseph Tatum <josephtatum@gmail.com>
2019-12-16 13:51:40 -0800

    created itenerary item model and wrote 2 passing tests. tied it to trip model as well

--- __tests__/trips.test.js
+++ __tests__/trips.test.js
@@ -4,10 +4,24 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Trip = require('../lib/models/Trip');
+const IteneraryItem = require('../lib/models/IteneraryItem');
 
 describe('app routes', () => {
   beforeAll(() => {
     connect();
+  });
+
+  let trip;
+  let iteneraryItem;
+  beforeEach(() => {
+    trip = new Trip({
+      origin: 'Portland, OR, USA',
+      destination: 'Stockholm, Sweden',
+      dateOfDeparture: new Date('2020-10-10'),
+      dateOfReturn: new Date('2020-10-30'),
+      modeOfTransit: 'airplane'
+    });
   });
 
   beforeEach(() => {
